069f4d3eab8f83310507eed83440b45a70cdc52a
MUSAMHLANGA <wsmhlanga1@gmail.com>
2021-06-06 16:37:05 +0200

    clean code

--- src/main/java/com/walter/wonderlabzassesment/repository/AccountSearchRepository.java
+++ src/main/java/com/walter/wonderlabzassesment/repository/AccountSearchRepository.java
@@ -1,7 +1,6 @@
 package com.walter.wonderlabzassesment.repository;
 
 import com.walter.wonderlabzassesment.model.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,8 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface AccountSearchRepository extends CrudRepository<BankAccount,Long> {
 
     @Query(value="SELECT * FROM BANK WHERE ACCOUNT_NUMBER = ? ",nativeQuery = true)
-    public BankAccount findByTech(String accountNum);
+     BankAccount findByTech(String accountNum);
 
-    @Query(value="UPDATE BANK SET ACCOUNT_BALANCE = accountBalance WHERE ACCOUNT_NUMBER = accountNum ",nativeQuery = true)
-    public BankAccount updateAccount(BankAccount bankAccount);
 }
